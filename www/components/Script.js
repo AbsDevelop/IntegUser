8330ed038c4595eb6970dc70ebfab73d76d09531
AbsDevelop <abnergmtomaz@gmail.com>
2021-08-31 04:10:16 +0900

    Project's Initial Commit

--- www/components/Script.js
+++ www/components/Script.js
@@ -1,26 +1,85 @@
 // This is a JavaScript file
 
-window.onload = function(){
-  const buscar = document.querySelector("#buscar");
-  const cep = document.querySelector("#cep");
+//Ação de Cadastro
+$(document).on("click", "#cadastrar", function(){
+  let parametros = {
+    "Nome": $('#nome').val(),
+    "Curso": $('#curso').val()
+  };
 
-  const opcoes = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'default'
-  }
+  $.ajax({
+    type: "post", //Como vou enviar
+    url: "https://www.jussimarleal.com.br/exemplo_api/pessoa", //Para onde vou enviar
+    data: parametros,
+    success: function(data) //Execução bem sucedida
+    {
+      alert(data);
+      $("#nome").val("");
+      $("#curso").val("");
+    },
+    error: function(data) //Execução mal sucedida
+    {
+      alert("Erro ao cadastrar suas informações!");
+    }
+  })
+});
 
-  buscar.addEventListener("click", function(){
-    fetch(`http://viacep.com.br/ws/${ cep.value }/json/`, opcoes)
-      .then(response => {response.json()
-        .then(data => {
-          document.querySelector("#CEP").value = data['cep'];
-          document.querySelector("#Logradouro").value = data['logradouro'];
-          document.querySelector("#Bairro").value = data['bairro'];
-          document.querySelector("#Localidade").value = data['localidade'];
-          document.querySelector("#UF").value = data['uf'];
-          document.querySelector("#DDD").value = data['ddd'];
-        })
-      })
-  });
-}
+//Ação de Busca
+$(document).on("click", "#buscar", function(){
+  $.ajax({
+    type: "get", //Como vou enviar
+    dataType: "json",
+    url: "https://www.jussimarleal.com.br/exemplo_api/pessoa/"+$("#id").val(), //Para onde vou enviar
+    success: function(data) //Execução bem sucedida
+    {
+      $("#nome").val(data.nome);
+      $("#curso").val(data.curso);
+    },
+    error: function(data) //Execução mal sucedida
+    {
+      alert("Erro ao encontrar seu ID!");
+    }
+  })
+});
+
+//Ação de Alteração
+$(document).on("click", "#alterar", function(){
+  let parametros = {
+    "Nome": $('#nome').val(),
+    "Curso": $('#curso').val()
+  };
+
+  $.ajax({
+    type: "put", //Como vou enviar
+    url: "https://www.jussimarleal.com.br/exemplo_api/pessoa/"+$("#id").val(), //Para onde vou enviar
+    data: parametros,
+    success: function(data) //Execução bem sucedida
+    {
+      alert(data);
+      $("#nome").val("");
+      $("#curso").val("");
+    },
+    error: function(data) //Execução mal sucedida
+    {
+      alert("Erro ao alterar as informações!");
+    }
+  })
+});
+
+//Ação de Exclusão
+$(document).on("click", "#deletar", function(){
+  $.ajax({
+    type: "delete", //Como vou enviar
+    url: "https://www.jussimarleal.com.br/exemplo_api/pessoa/"+$("#id").val(), //Para onde vou enviar
+    success: function(data) //Execução bem sucedida
+    {
+      alert(data);
+      $("#nome").val("");
+      $("#curso").val("");
+    },
+    error: function(data) //Execução mal sucedida
+    {
+      alert("Erro ao excluir as informações!");
+    }
+  })
+});
